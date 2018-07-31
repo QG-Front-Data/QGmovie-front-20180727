6e7c54df0c8f1855a1ae1c4801cd40e208350577
ancs <zxc365912613@qq.com>
2018-07-31 10:06:00 +0800

    修改2

--- QGmovie/scripts/util.js
+++ QGmovie/scripts/util.js
@@ -1,5 +1,6 @@
 /**
  * 对表单的输入字符个数进行限制，超出的话截取前number个字符作为这个表单的值
+ * @func
  * @param {*} input 输入表单的对象
  * @param {*} number 输入的字数限制
  */
@@ -12,14 +13,16 @@ function inputLimit(input, number) {
 
 /**
  * 这是发送所有请求的方法，所有的servlet请求都要经过这个函数
+ * @func
  * @param {String} serverAddress 服务器地址
  * @param {String} method 请求方式
  * @param {String} sendData 要发送的数据
  * @param {String} sendDataType 数据类型
  * @param {String} contentType 请求头
- * @param {Function} callback 完成后的执行内容，第一个参数是请求对象，第二个参数是状态码 
+ * @param {Function} successCallback 请求成功后的执行函数。两个函数的第一个参数都是请求对象，第二个参数都是状态码
+ * @param {Function} errorCallback 请求失败时候的执行函数。
  */
-function ajaxRequest(serverAddress, method, sendData, sendDataType, contentType , callback) {
+function ajaxRequest(serverAddress, method, sendData, sendDataType, contentType , successCallback, errorCallback) {
     $.ajax({
     	url: serverAddress,
     	type: method,
@@ -27,12 +30,14 @@ function ajaxRequest(serverAddress, method, sendData, sendDataType, contentType 
         dataType: sendDataType,
     	processData: false,
     	contentType: contentType,
-        complete: callback
+        success: successCallback,
+        error: errorCallback
     	});
 }
 
 /**
  * 当测试结果不为空，则返回true，当测试结果为空的时候，则返回false。
+ * @func
  * @param {RegExp} pattern 正则表达式
  * @param {String} text 要验证的正则表达式
  */
@@ -42,6 +47,7 @@ function RegExpTest(pattern, text) {
 
 /**
  * 这是一个兼容性的监听事件。只需要直接用这个对象的方法就行。惰性加载函数，所以控制台输出只能是当前浏览器支持的监听事件
+ * @event
  * @function EventUtil.addHandler 全局的添加事件的方法。
  * @function EventUtil.removeHandler 全局的删除事件的方法。
  * @param {Object} element 添加事件的对象
